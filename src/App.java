d95542cb5da1b9652bb4761b94e7278f498bfd86
Chrissisx <chriscriollo2309@gmail.com>
2024-05-25 23:52:06 -0500

    inicioRepósitorio

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //Declarar
+
+        //Instanciar
+
+        System.out.println("*********************************");
+        System.out.println("*                               *");
+        System.out.println("*            GRUPO 1            *");
+        System.out.println("*                               *");
+        System.out.println("*********************************");
+        System.out.println("   Integrantes: "+"\n - Jairo Angulo"+"\n - Joan Bedon"+"\n - Camila Caicedo"+"\n - Estefano Condoy"+"\n - Christopher Criollo");
+        System.out.println("\n*********************************");
+        
+    }
+}
