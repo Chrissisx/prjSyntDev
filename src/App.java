00caa85f1406f591db8e68f91c4e5474b3333a5a
Chrissisx <chriscriollo2309@gmail.com>
2024-05-27 12:36:35 -0500

    correciónNombres2.0

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        //Declarar'¿
+        //Declarar
         //Instanciar
         System.out.println("*********************************");
         System.out.println("*                               *");
