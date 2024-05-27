6a6c0e65527ad0c179c3a1622fa2603d28ab1934
Chrissisx <chriscriollo2309@gmail.com>
2024-05-27 15:55:36 -0500

    ConfiguracionAlMain

--- src/App.java
+++ src/App.java
@@ -22,23 +22,31 @@ public class App {
         
 
         // Metodo
+
+        //**************************SERIE NUMERICA**************************
+        //SN12
+        System.out.println();
         oCCSN12.SerieNumerica12For(numeroPos);
         System.out.println();
         oCCSN12.SerieNumerica12doWhile(numeroPos);
         System.out.println();
         oCCSN12.SerieNumerica12While(numeroPos);
         System.out.println();
+        //**************************SERIE CARATERES**************************
+        //SC7
         oCCSC7.SerieCaracter7For(numeroPos);
         System.out.println();
         oCCSC7.SerieCaracter7doWhile(numeroPos);
         System.out.println();
         oCCSC7.SerieCaracter7While(numeroPos);
         System.out.println();
+        //SC8
         oCCSC8.SerieCaracter8For(numeroPos);
         System.out.println();
         oCCSC8.SerieCaracter8doWhile(numeroPos);
         System.out.println();
         oCCSC8.SerieCaracter8While(numeroPos);
+        //******************************FIGURAS******************************
 
     }
 }
